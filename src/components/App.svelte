bc3369385cd245b08505ae4fc7e9871c4e40fba2
Adalina Ma <adalinama@Adalinas-Air.lan>
2024-05-07 15:20:13 -0700

    added map

--- src/components/App.svelte
+++ src/components/App.svelte
@@ -1,13 +1,68 @@
 <script>
-  // Write your JS here, or import other files
+  import { onMount } from 'svelte';
+  import * as d3 from 'd3';
+
+  let svg;
+
+  onMount(() => {
+    const width = window.innerWidth;
+    const height = window.innerHeight;
+
+    // The svg
+    svg = d3.select("#my_dataviz")
+      .attr("width", width)
+      .attr("height", height);
+
+    // Map and projection
+    const projection = d3.geoNaturalEarth1()
+      .scale(width / 1.3 / Math.PI)
+      .translate([width / 2, height / 2]);
+
+    // Load external data and draw the map
+    d3.json("https://raw.githubusercontent.com/holtzy/D3-graph-gallery/master/DATA/world.geojson")
+      .then(data => {
+        // Draw the map
+        svg.append("g")
+          .selectAll("path")
+          .data(data.features)
+          .enter().append("path")
+            .attr("fill", "#69b3a2")
+            .attr("d", d3.geoPath()
+              .projection(projection)
+            )
+            .style("stroke", "#fff")
+            .on("mouseover", handleMouseOver)
+            .on("mouseout", handleMouseOut);
+      });
+  });
+
+  function handleMouseOver(event, d) {
+    d3.select(this)
+      .attr("fill", "orange");
+  }
+
+  function handleMouseOut(event, d) {
+    d3.select(this)
+      .attr("fill", "#69b3a2");
+  }
 </script>
 
 <main>
-  <h1>Svelte template</h1>
-
-  <p>Write your HTML here</p>
+  <svg id="my_dataviz"></svg>
 </main>
 
 <style>
-  /* Write your CSS here */
+  body, html {
+    margin: 0;
+    padding: 0;
+    width: 100%;
+    height: 100%;
+    overflow: hidden;
+  }
+
+  svg {
+    display: block;
+    width: 100%;
+    height: 100%;
+  }
 </style>
